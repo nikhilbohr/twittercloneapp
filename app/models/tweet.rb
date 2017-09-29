d28d3a1574fc7d042e9e7b357eba260fec728159
nikhilbohr <nikhil@hackerrank.com>
2017-09-29 16:58:58 +0530

    added models

---
+++ app/models/tweet.rb
@@ -0,0 +1,7 @@
+class Tweet < ApplicationRecord
+    belongs_to :user
+
+    default_scope -> { order(created_at: :desc) }
+      validates :content, presence: true, length: { maximum: 280 }
+      validates :user_id, presence: true
+end
